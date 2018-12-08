95d105122f16904d05b82b03a4f2bbb32be6418a
River <cdhriver@gmail.com>
2018-12-08 12:07:01 +0800

    Update install-docker.sh
    install unzip

--- install-docker.sh
+++ install-docker.sh
@@ -14,8 +14,9 @@ echo "{
 
 curl -L https://get.daocloud.io/docker/compose/releases/download/1.18.0/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose;
 chmod +x /usr/local/bin/docker-compose;
-curl -o webserver-dir-structure.zip https://codeload.github.com/myriver/webserver-dir-structure/zip/master
-unzip   webserver-dir-structure.zip
-rm -f   webserver-dir-structure.zip
-mv      webserver-dir-structure-master /data  
+curl -o webserver-dir-structure.zip https://codeload.github.com/myriver/webserver-dir-structure/zip/master;
+yum install -y unzip;
+unzip   webserver-dir-structure.zip;
+rm -f   webserver-dir-structure.zip;
+mv      webserver-dir-structure-master /data;  
 reboot;
