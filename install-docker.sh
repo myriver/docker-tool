3a0410fa9899191c294e0dfbc8213637bbf641af
River <cdhriver@gmail.com>
2019-03-19 11:02:06 +0800

    Update install-docker.sh
    resolve curl err "Peer reports incompatible or unsupported protocol version"

--- install-docker.sh
+++ install-docker.sh
@@ -1,6 +1,7 @@
 rpm --import https://www.elrepo.org/RPM-GPG-KEY-elrepo.org;
 rpm -Uvh http://www.elrepo.org/elrepo-release-7.0-2.el7.elrepo.noarch.rpm;
 yum install -y yum-plugin-fastestmirror;
+yum update -y nss curl libcurl;
 yum -y --enablerepo=elrepo-kernel install kernel-lt;
 grub2-set-default 0;
 curl -sSL https://get.daocloud.io/docker | sh;
